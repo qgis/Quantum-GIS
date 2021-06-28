501dc28735dfb438f83adce34a2f537b52cad2b3
Julien Cabieces <julien.cabieces@oslandia.com>
2021-06-28 17:05:53 +0200

    use unique_ptr for projectexternalstorage instance

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -2419,7 +2419,7 @@ QgsScaleBarRendererRegistry *QgsApplication::scaleBarRendererRegistry()
 
 QgsProjectStorageRegistry *QgsApplication::projectStorageRegistry()
 {
-  return members()->mProjectStorageRegistry;
+  return members()->mProjectStorageRegistry.get();
 }
 
 QgsExternalStorageRegistry *QgsApplication::externalStorageRegistry()
@@ -2566,7 +2566,7 @@ QgsApplication::ApplicationMembers::ApplicationMembers()
   }
   {
     profiler->start( tr( "Setup project storage registry" ) );
-    mProjectStorageRegistry = new QgsProjectStorageRegistry();
+    mProjectStorageRegistry.reset( new QgsProjectStorageRegistry() );
     profiler->end();
   }
   {
@@ -2623,7 +2623,6 @@ QgsApplication::ApplicationMembers::~ApplicationMembers()
   delete mPaintEffectRegistry;
   delete mPluginLayerRegistry;
   delete mProcessingRegistry;
-  delete mProjectStorageRegistry;
   delete mPageSizeRegistry;
   delete mAnnotationItemRegistry;
   delete mLayoutItemRegistry;
